8a45a4b569f256c2de274e5c41b9262932e0c14e
M ASAD SATTAR <asadjam400@gmail.com>
2026-04-19 21:59:39 +0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,29 @@
+num1=int(input("enter first number for subtraction:"))
+num2=int(input("enter second number for subtraction:"))
+def sub(num1,num2):
+    subt=num1-num2
+    return subt
+result=sub(num1,num2)
+print(result)
+num1=int(input("enter first number for multiplication:"))
+num2=int(input("enter second number for multiplication:"))
+def multiply(num1,num2):
+    mul=num1*num2
+    return mul
+result=multiply(num1,num2)
+print(result)
+# takes input from user and add them 
+num1=int(input("enter first number for adition:"))
+num2=int(input("enter second number for addition:"))
+def add(num1,num2):
+    sum=num1+num2
+    return sum
+result=add(num1,num2)
+print(result)
+num1=int(input("enter first number for division:"))
+num2=int(input("enter second number for  division:"))
+def divide(num1,num2):
+    div=num1/num2
+    return div 
+result=divide(num1,num2)
+print(result)
